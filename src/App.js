7dd52e9e9152704ab809362097d42a32d7b0011f
mkpietrzyk <mkm.pietrzyk@gmail.com>
2019-11-06 14:21:51 +0100

    initial deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {CSSTransition, SwitchTransition} from "react-transition-group";
 import './App.css';
 
+
 function App() {
+  const [objOneIn, setObjOneIn] = useState(true);
+
+  const goForFurtherDetails = () => {
+    setObjOneIn(false)
+  };
+
+  const goBack = () => {
+    setObjOneIn(true)
+  };
+
+  const FadeTransition = props => {
+    console.log('should enter from forward and exit from back:', props.moveForward);
+    return (
+      <CSSTransition
+        {...props}
+        classNames={{
+          enter: props.moveForward ? 'fade-enter-back' : 'fade-enter-forward',
+          enterActive: "fade-enter-active",
+          exit: props.moveForward ? 'fade-exit-back' : 'fade-exit-forward'
+        }}
+        addEndListener={(node, done) => {
+          node.addEventListener("transitionend", done, false);
+        }}
+      />
+    )
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SwitchTransition>
+          <FadeTransition
+            key={objOneIn}
+            moveForward={objOneIn}
+          >
+            {objOneIn ? (
+              <div style={{width: '450px', height: '150px'}}>
+                <span>Some details page 1</span>
+                <button onClick={goForFurtherDetails}>
+                  go further
+                </button>
+              </div>
+            ) : (
+              <div style={{width: '450px', height: '150px'}}>
+                <span>Further details page</span>
+                <button onClick={goBack}>
+                  go back
+                </button>
+              </div>
+            )}
+          </FadeTransition>
+        </SwitchTransition>
       </header>
     </div>
   );
